f4f3e31b8efe90a54e123a038d444208565ae249
Dina Hidayatul Rasidah <dina.hidayatul.9@gmail.com>
2026-05-15 19:30:41 +0700

    update

--- coba/koneksi.php
+++ coba/koneksi.php
@@ -2,10 +2,12 @@
 $host = "localhost";
 $user = "root";
 $pass = "";
-$db   = "krs";
+$db   = "krss";
 
 $konek = mysqli_connect($host, $user, $pass, $db);
 
+
 if (!$konek) {
     die("Koneksi gagal: " . mysqli_connect_error());
-}
+    }
+?>
